4d726c100ee017307fdb1207d7ba3ec3e282a8ed
hmg <hmg@kali>
2024-03-13 08:20:21 +0100

    exo3

---
+++ TP1/src/exo3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int X;
+    X = 5%2;
+    printf("X = %d\n", X);
+    float Y;
+    Y = 5.0/2;
+    printf("X = %f\n", Y);
+    return 0;
+}
