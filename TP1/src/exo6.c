f88298c0020864f1d20e001e8a5e14da26c920fd
hmg <hmg@kali>
2024-03-13 10:28:22 +0100

    exo6

--- TP1/src/exo6.c
+++ TP1/src/exo6.c
@@ -8,30 +8,34 @@ int main() {
     scanf(" %c", &op);
 
     while (op != 's') { 
-        printf("Entrez le premier nombre : ");
-        scanf("%f", &a);
-        printf("Entrez le deuxième nombre : ");
-        scanf("%f", &b);
-        switch (op) {
-            case '/':
-                a = a / b;
-                break; 
-            case '*':
-                a = a * b;
-                break;
-            case '-':
-                a = a - b;
-                break;
-            case '+':
-                a = a + b;
-                break;
-            default:
-                printf("Opération invalide\n");
-                break;
+        if (op != '/' && op != '*' && op != '-' && op != '+') { 
+            printf("Opération invalide\n");
+        } else {
+            printf("Entrez le premier nombre : ");
+            scanf("%f", &a);
+            printf("Entrez le deuxième nombre : ");
+            scanf("%f", &b);
+            switch (op) {
+                case '/':
+                    if (b != 0) 
+                        a = a / b;
+                    else
+                        printf("Attention : Division par zéro\n");
+                    break; 
+                case '*':
+                    a = a * b;
+                    break;
+                case '-':
+                    a = a - b;
+                    break;
+                case '+':
+                    a = a + b;
+                    break;
+            }
+            printf("Le résultat est : %.2f\n", a); 
         }
-        printf("Le résultat est : %.2f\n", a); 
         printf("Entrez l'opération  (/, *, -, +, s pour stopper) : "); 
+        scanf(" %c", &op);
     }
-
     return 0;
 }
