0315ac8d55058252dd33ca1a8ec26d23e52d0a8b
Hugo <hugolenoe@gmail.com>
2024-03-18 11:53:09 +0100

    Co-authored-by: Hamza <Som-gif@users.noreply.github.com>
    exo 2

--- TP2/src/exo2.c
+++ TP2/src/exo2.c
@@ -4,10 +4,9 @@
 #include <time.h>
 
 
-void triBulle(int tab[]){
+void triBulle(int tab[], int taille){
     bool noModif = false;
     int temp = 0;
-    int taille = 50;
     while (!noModif)
     {
         noModif = true;
@@ -28,7 +27,6 @@ void triBulle(int tab[]){
         printf("%d ", tab[i]);
     }
 
-    
 }
 
 
@@ -52,31 +50,43 @@ void triMinimaSucc(int tab[], int taille){
     }
 }
 
+void triInsertion(int tab[], int taille){
+    int i, j;
+    int en_cours;
+ 
+    for (i = 1; i < taille; i++) {
+        en_cours = tab[i];
+        for (j = i; j > 0 && tab[j - 1] > en_cours; j--) {
+            tab[j] = tab[j - 1];
+        }
+        tab[j] = en_cours;
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    int tableau[50];
-    int tableau2[50];
+    int tableau[50], tableau2[50], tableau3[50];
     int i;
 
     srand(time(NULL));
     for (i = 0; i < 50; i++) {
         tableau[i] = rand() % 100; 
         tableau2[i] = rand() % 100;
+        tableau3[i] = rand() % 100;
+
     }
 
-    printf("Tableau de 50 entiers :\n\n");
+    printf("Tableau de 50 entiers :\n");
         for (int i = 0; i < 50; i++) {
         printf("%d ", tableau[i]);
     }
     printf("\n");
     printf("Tri à bulle:\n");
-    triBulle(tableau);
-    printf("\n");
-    printf("\n");
+    triBulle(tableau, 50);
+    printf("\n\n");
 
 
-    printf("Tableau de 50 entiers :\n\n");
+    printf("Tableau de 50 entiers :\n");
         for (int i = 0; i < 50; i++) {
         printf("%d ", tableau2[i]);
     }
@@ -86,5 +96,20 @@ int main(int argc, char *argv[])
     for (int i = 0; i < 50; i++) {
         printf("%d ", tableau2[i]);
     }
+
+
+    printf("\n\n");
+    printf("Tableau de 50 entiers :\n");
+        for (int i = 0; i < 50; i++) {
+        printf("%d ", tableau3[i]);
+    }
+    printf("\n");
+    printf("Tri Insertion:\n");
+    triInsertion(tableau3, 50);
+    for (int i = 0; i < 50; i++) {
+        printf("%d ", tableau3[i]);
+    }
+
+    printf("\n");
 }
 
